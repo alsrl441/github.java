d0087cc5f431d08d82c5ccb85b8896717f6f1594
mingi <128763318+alsrl441@users.noreply.github.com>
2023-09-08 18:14:28 +0900

    Sorry.java

---
+++ Sorry.java
@@ -0,0 +1,10 @@
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sorry~~");
+    System.out.print("재밌어서 미안합니다~~");
+	}
+
+}
